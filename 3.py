dfc47e86e772771af4e60caf5e0b8336d9aaa7ee
farha <Farha.Mim@metropolia.fi>
2024-08-22 11:16:43 +0300

    loppu

---
+++ 3.py
@@ -0,0 +1,7 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+
+print(f"Suorakulmion piiri on {piiri:.2f} ja pinta-ala on {pinta_ala:.2f} neliöyksikköä")
