ba5e53682e96d08894a97f213e26a495ed70407f
farha <Farha.Mim@metropolia.fi>
2024-08-22 11:48:14 +0300

    loppu

---
+++ 4.py
@@ -0,0 +1,9 @@
+luku1=int(input("anna ensimäinen kokonaisluku"))
+luku2=int(input("anna toinen kokonaisluku"))
+luku3=int(input("anna kolmas kokonaisluku"))
+
+summa =luku1+luku2+luku3
+tulo=luku1*luku2*luku3
+keskiarvo=summa/3
+
+print(f"lukuen summa on {summa},tulo on {tulo},keskiarvo on {keskiarvo:.2f}")
